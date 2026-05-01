af1a580579ca7fa5b1d5d479ef150f2e5686b230
kl2400033283 <2400033283@kluniversity.in>
2026-05-01 19:23:31 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from "react-route
 import { useEffect } from "react";
 
 // Existing Core Pages
+import OAuth2Callback from "./Pages/OAuth2Callback";
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs"; 
 import Login from "./Pages/Login";
@@ -12,7 +13,7 @@ import Attraction from "./Pages/Attraction";
 import Plan from "./Pages/Plan";
 import Homestay from "./Pages/Homestay";
 import Guide from "./Pages/Guide";
-import OAuth2Callback from "./Pages/OAuth2Callback";
+
 
 // Dashboard Components
 import TouristDashboard from "./Pages/TouristDashboard";
